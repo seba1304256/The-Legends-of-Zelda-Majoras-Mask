0bd0ee83aaa0516ab95966ef89637e16d1cf3be2
Albedo-AB <di.aravenab@duocuc.cl>
2026-08-27 18:26:53 -0400

    avances

---
+++ src/Mascaras.java
@@ -0,0 +1,14 @@
+public class Mascaras {
+
+    String instrumento;
+    int vida;
+    int puntos_ataque;
+    String area_de_ataque;
+    int altura;
+    String tipo;
+    int peso;
+    String velocidad;
+    String fuerza;
+    boolean nadar;
+    
+}
